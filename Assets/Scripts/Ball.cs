ea8240a6ea46360596d6bf9d52cee785018f198a
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-11 13:44:55 +0200

    Add script for ball and ball material

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField]
+    float InitialSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * InitialSpeed;
+    }
+}
