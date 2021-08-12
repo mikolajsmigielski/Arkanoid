864efbdd5529a5693081c670d22a739b6d0dcfa1
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-12 18:03:40 +0200

    Added possibility to lose the game

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class DeadZone : MonoBehaviour
+{
+    [SerializeField]
+    string LevelName = "Menu";
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ball>() == null) return;
+        SceneManager.LoadScene(LevelName);
+    }
+}
