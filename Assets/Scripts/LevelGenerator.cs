3833eb71fc1df7e84e97d19d0c5a376b2de440e8
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-20 15:22:34 +0200

    Level generation has been improved.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -8,20 +10,29 @@ public class LevelGenerator : MonoBehaviour
     GameObject BlockPrefab;
     void Start()
     {
-        GenerateLevel(5, 3);
+        
         LoadLevel("First_Level");
     }
     void LoadLevel(string name)
     {
-        var text = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Levels/" + name + ".txt");
-        Debug.Log(text);
-    }
-    void GenerateLevel(int width, int height)
+        var path = "Assets/Levels/" + name + ".txt";
+        var text = AssetDatabase.LoadAssetAtPath<TextAsset>(path).text;
+        var lines = Regex.Split(text, System.Environment.NewLine);
+        GenerateLevel(lines);
+     }
+    void GenerateLevel(string[] text)
     {
+        var height = text.Length;
+        var width = text[0].Length;
+
         for(int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
+                var character = text[y][x];
+                if (character == '_')
+                    continue;
+
                 var position = new Vector2(x - width / 2, - y + height / 2);
                 GenerateBlock(position);
             }
