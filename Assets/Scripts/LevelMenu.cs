f00874c50f7a2b752bd8c1bc43205869f79d28b9
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-20 19:11:57 +0200

    Created menu

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    [SerializeField]
+    string LevelName;
+    
+    void Start()
+    {
+        GetComponentInChildren<Text>().text = LevelName;
+        GetComponent<Button>().onClick.AddListener(ChangeScene);
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+}
