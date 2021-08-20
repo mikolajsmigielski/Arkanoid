39d56ae4517b4437d19ceacea8f28d09fde15fd3
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-08-20 18:05:29 +0200

    Improved ManagerOfGame script

--- Assets/Scripts/ManagerOfGame.cs
+++ Assets/Scripts/ManagerOfGame.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ManagerOfGame : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        FindObjectOfType<LevelGenerator>().GenerateLevel("First_Level"); 
+        FindObjectOfType<LevelGenerator>().GenerateLevel("First_Level");
+        StartCoroutine(CheckIfLevelEndCorutine());
+    }
+    IEnumerator CheckIfLevelEndCorutine()
+    {
+        while (true)
+        {
+            CheckIfLevelEnd();
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
+    void CheckIfLevelEnd()
+    {
+        var numberOfBlocks = FindObjectsOfType<Block>().Length;
+        if (numberOfBlocks == 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
